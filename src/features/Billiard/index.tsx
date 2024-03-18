89f69efd038c462caacb596843bea6965832476d
Solpio <ssusolpio@gmail.com>
2024-03-19 03:57:23 +0400

    add handlers

--- src/features/Billiard/index.tsx
+++ src/features/Billiard/index.tsx
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Canvas from "shared/Canvas";
 
+const friction = 0.99;
 interface Ball {
 	x: number;
 	y: number;
@@ -16,10 +17,27 @@ interface BilliardProps {
 	height: number;
 }
 
+const borderCollision = (ball: Ball, canvas: HTMLCanvasElement) => {
+	if (
+		ball.y + ball.radius + ball.vy > canvas.height ||
+		ball.y - ball.radius + ball.vy < 0
+	) {
+		ball.vy = -ball.vy;
+	}
+	if (
+		ball.x + ball.radius + ball.vx > canvas.width ||
+		ball.x - ball.radius + ball.vx < 0
+	) {
+		ball.vx = -ball.vx;
+	}
+};
+
 const draw = (
 	ctx: CanvasRenderingContext2D | null,
 	balls: Ball[],
-	canvas: HTMLCanvasElement
+	canvas: HTMLCanvasElement,
+	cueStickStart: { x: number; y: number } | null,
+	cueStickEnd: { x: number; y: number } | null
 ) => {
 	if (!ctx) return;
 	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -29,27 +47,37 @@ const draw = (
 	ctx.stroke();
 
 	balls.forEach((ball) => {
+		ball.vx *= friction;
+		ball.vy *= friction;
+
 		ball.x += ball.vx;
 		ball.y += ball.vy;
-		if (
-			ball.y + ball.radius + ball.vy > canvas.height ||
-			ball.y - ball.radius + ball.vy < 0
-		) {
-			ball.vy = -ball.vy;
-		}
-		if (
-			ball.x + ball.radius + ball.vx > canvas.width ||
-			ball.x - ball.radius + ball.vx < 0
-		) {
-			ball.vx = -ball.vx;
-		}
+
+		borderCollision(ball, canvas);
 		ball.draw(ctx);
 	});
+
+	if (cueStickStart && cueStickEnd) {
+		ctx.beginPath();
+		ctx.moveTo(cueStickStart.x, cueStickStart.y);
+		ctx.lineTo(cueStickEnd.x, cueStickEnd.y);
+		ctx.strokeStyle = "white";
+		ctx.stroke();
+	}
 };
 
 const Billiard: React.FC<BilliardProps> = ({ width, height }) => {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const [cueStickStart, setCueStickStart] = useState<{
+		x: number;
+		y: number;
+	} | null>(null);
+	const [cueStickEnd, setCueStickEnd] = useState<{
+		x: number;
+		y: number;
+	} | null>(null);
 	const rafRef = useRef<number>(0);
+	const clickedIndex = useRef(-1);
 
 	const ballsRef = useRef<Ball[]>([
 		{
@@ -75,12 +103,67 @@ const Billiard: React.FC<BilliardProps> = ({ width, height }) => {
 
 		if (!canvas || !ctx) return;
 		const animationLoop = () => {
-			draw(ctx, ballsRef.current, canvas);
+			draw(ctx, ballsRef.current, canvas, cueStickStart, cueStickEnd);
 			rafRef.current = requestAnimationFrame(animationLoop);
 		};
-
+		console.log(cueStickStart, cueStickEnd);
 		animationLoop();
 		return () => {
+			cancelAnimationFrame(rafRef.current);
+		};
+	}, [cueStickStart, cueStickEnd]);
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		const ctx = canvas?.getContext("2d");
+
+		if (!canvas || !ctx) return;
+		const handleMouseDown = (event: MouseEvent) => {
+			const rect = canvas.getBoundingClientRect();
+			const mouseX = event.clientX - rect.left;
+			const mouseY = event.clientY - rect.top;
+
+			const clickedBallIndex = ballsRef.current.findIndex((ball) => {
+				const dx = ball.x - mouseX;
+				const dy = ball.y - mouseY;
+				return Math.sqrt(dx * dx + dy * dy) <= ball.radius;
+			});
+			if (clickedBallIndex !== -1) {
+				clickedIndex.current = clickedBallIndex;
+				ballsRef.current[clickedBallIndex].vy = 0;
+				ballsRef.current[clickedBallIndex].vx = 0;
+				setCueStickStart({
+					x: ballsRef.current[clickedBallIndex].x,
+					y: ballsRef.current[clickedBallIndex].y,
+				});
+			}
+			canvas.addEventListener("mousemove", handleMouseMove);
+		};
+
+		const handleMouseMove = (event: MouseEvent) => {
+			const rect = canvas.getBoundingClientRect();
+			const mouseX = event.clientX - rect.left;
+			const mouseY = event.clientY - rect.top;
+			setCueStickEnd({
+				x: mouseX,
+				y: mouseY,
+			});
+		};
+
+		const handleMouseUp = () => {
+			if (clickedIndex.current > -1) {
+				clickedIndex.current = -1;
+				setCueStickStart(null);
+				setCueStickEnd(null);
+			}
+			canvas.removeEventListener("mousemove", handleMouseMove);
+		};
+		canvas.addEventListener("mouseup", handleMouseUp);
+		canvas.addEventListener("mousedown", handleMouseDown);
+		return () => {
+			canvas.removeEventListener("mouseup", handleMouseUp);
+			canvas.removeEventListener("mousemove", handleMouseMove);
+			canvas.removeEventListener("mousedown", handleMouseDown);
 			cancelAnimationFrame(rafRef.current);
 		};
 	}, []);
